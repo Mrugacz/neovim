2e9115f02e8c221466fedbbfb6cdb59045eb6fd8
mrugacz <mrugalski.kuba@gmail.com>
2024-01-22 21:07:00 +0100

    added initial config. based on: https://www.youtube.com/playlist?list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn

---
+++ lua/vim-options.lua
@@ -0,0 +1,16 @@
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- tab width
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+-- expand tabs into spaces
+vim.opt.expandtab = true
+-- disable word splitting
+vim.opt.linebreak = true
+
+-- set min lines from edge
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.g.mapleader = " "
