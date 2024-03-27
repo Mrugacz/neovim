478050d1df65721c71ffc2cebbfef34c077267ef
mrugacz <mrugalski.kuba@gmail.com>
2024-03-27 21:25:06 +0100

    updated language settings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,57 +1,70 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-				settings = {
-					pylsp = {
-						plugins = {
-							pycodestyle = {
-								maxLineLength = 200,
-							},
-						},
-					},
-				},
-			})
-			lspconfig.csharp_ls.setup({
-				capabilities = capabilities,
-			})
+            local lspconfig = require("lspconfig")
+            -- JS
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            -- LUA
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            -- HTML
+            lspconfig.html.setup({
+                capabilities = capabilities,
+            })
+            -- BASH
+            lspconfig.bashls.setup({
+                capabilities = capabilities,
+            })
+            -- PYTHON
+            lspconfig.pylsp.setup({
+                capabilities = capabilities,
+                settings = {
+                    pylsp = {
+                        plugins = {
+                            pycodestyle = {
+                                maxLineLength = 200,
+                            },
+                        },
+                    },
+                },
+            })
+            -- C#
+            lspconfig.csharp_ls.setup({
+                capabilities = capabilities,
+            })
+            -- GO
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.golangci_lint_ls.setup({
+                capabilities = capabilities,
+            })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
